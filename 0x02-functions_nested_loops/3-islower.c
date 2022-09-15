9f508e067442f1f96c0264c0648d94053feff1cc
Billiah Kerubo <kbmenyenya@gmail.com>
2022-09-15 08:01:15 -0700

    3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,21 +1,11 @@
-#include <stdio.h>
+#include "main.h"
+
 int _islower(int c)
 {
 	if (c >= 'a' && c<= 'z')
 	       return (1);
 	else 
 		return (0);	
-}
-int main (void)
-{
-	int r;
-	r = _islower('H');
-	putchar(r + '0');
-	r = _islower('o');
-	putchar(r + '0');
-	r = _islower(108);
-	putchar(r + '0');
-	putchar('\n');
-	return (0);
+
 }
 
