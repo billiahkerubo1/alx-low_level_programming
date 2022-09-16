7de35b7f4e94aa90ab8a139f5d9c5c4d7446adbe
Billiah Kerubo <kbmenyenya@gmail.com>
2022-09-16 08:43:01 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_most_numbers(void)
+{
+	int num;
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+			_putchar((num % 10) + '0');
+	}
+	_putchar('\n');
+}
